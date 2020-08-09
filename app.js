4f15008522036fa5ce12bd338f4e6c1744d7b6cc
Pigrabbit <donghyuk1003@gmail.com>
2020-08-09 20:45:58 +0900

    Add mock data

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
-const app = express();
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql')
+
+const app = express()
+
+const { user, item, order, orderItem } = require("./src/data");
 
 const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: "Hello_world",
-        fields: () => ({
-            message: {
-                 type: GraphQLString,
-                 resolve: () => "Hello world!"
-            }
-        })
+  // query를 정의
+  query: new GraphQLObjectType({
+    name: 'Hello_world',
+    // 여러가지 field를 두어 query가능: category, name, rating 등
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello world!'
+      }
     })
+  })
 })
 
-
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
-    schema: schema,
-    graphiql: true,
+    schema,
+    graphiql: true
   })
-);
+)
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("graphql server running!");
-});
+  console.log('graphql server running!')
+})
---
+++ src/data.js
@@ -0,0 +1,64 @@
+const item = [
+  {
+    id: 1, name: '신라면', price: '1000', category: '라면'
+  },
+  {
+    id: 2, name: '월드콘', price: '1500', category: '아이스크림'
+  },
+  {
+    id: 3, name: '돼지바', price: '1000', category: '아이스크림'
+  },
+  {
+    id: 4, name: '초코첵스', price: '6000', category: '시리얼'
+  },
+  {
+    id: 5, name: '서울우유', price: '3000', category: '우유'
+  },
+  {
+    id: 6, name: '스윙칩', price: '2000', category: '과자'
+  },
+  {
+    id: 7, name: '몽쉘', price: '3500', category: '과자'
+  },
+  {
+    id: 8, name: '짜파게티', price: '1000', category: '라면'
+  },
+  {
+    id: 9, name: '바나나우유', price: '1200', category: '우유'
+  },
+  {
+    id: 10, name: '붕어싸만코', price: '1500', category: '아이스크림'
+  }
+]
+
+const order = [
+  { id: 1, user_id: 1 },
+  { id: 2, user_id: 2 },
+  { id: 3, user_id: 1 },
+  { id: 4, user_id: 3 }
+]
+
+const user = [
+  { id: 1, name: '이승엽' },
+  { id: 2, name: '류현진' },
+  { id: 3, name: '오승환' }
+]
+
+const orderItem = [
+  { id: 1, order_id: 1, item_id: 1 },
+  { id: 2, order_id: 1, item_id: 8 },
+  { id: 3, order_id: 2, item_id: 1 },
+  { id: 4, order_id: 2, item_id: 2 },
+  { id: 5, order_id: 2, item_id: 7 },
+  { id: 6, order_id: 2, item_id: 9 },
+  { id: 7, order_id: 4, item_id: 5 },
+  { id: 8, order_id: 3, item_id: 5 },
+  { id: 8, order_id: 3, item_id: 4 }
+]
+
+module.exports = {
+  user,
+  item,
+  order,
+  orderItem
+}
